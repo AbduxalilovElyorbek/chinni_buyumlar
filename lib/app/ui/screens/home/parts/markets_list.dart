2159df592a76dcc397c556f95a43b03a21549aa6
Elyorbek Abdukhalilov <elyorbekabduxalilov2007@gmail.com>
2024-04-09 15:46:03 +0300

    updated

--- lib/app/ui/screens/home/parts/markets_list.dart
+++ lib/app/ui/screens/home/parts/markets_list.dart
@@ -20,19 +20,23 @@ class MarketsListWidget extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Text(
-                title,
-                style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                      color: green,
-                      fontSize: 20.sp,
-                      fontWeight: FontWeight.w700,
-                    ),
+              SizedBox(
+                width: 220.w,
+                child: Text(
+                  title,
+                  maxLines: 2,
+                  style: Theme.of(context).textTheme.titleSmall!.copyWith(
+                        color: green,
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.w700,
+                      ),
+                ),
               ),
               ElevatedButton(
                 onPressed: func,
                 style: ElevatedButton.styleFrom(
                   backgroundColor: transparent,
-                  padding: EdgeInsets.symmetric(vertical: 6.h),
+                  padding: EdgeInsets.symmetric(vertical: 6.h,horizontal: 20.w),
                   elevation: 0,
                   side: BorderSide(
                     color: green,
